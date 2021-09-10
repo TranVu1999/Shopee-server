55a2d33f5e80bc571ab3c8391b728cb3b5854e72
TranVu1999 <tranvudpqn123@gmail.com>
2021-09-10 20:48:10 +0700

    10/09/2021

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,7 +1,11 @@
 // Models
 const Shop = require("./../models/shop");
+const Account = require("./../models/account");
 const User = require("./../models/user");
 
+// Module
+const Format = require('./../utils/format');
+const Validate = require('./../utils/validate');
 
 module.exports = {
     /**
@@ -53,4 +57,88 @@ module.exports = {
             })
         }
     },
+
+    /**
+     * Put information of shop
+    */
+    updateInformation: async function(req, res){        
+        const {accountId, role} = req;
+        let {
+            avatar,
+            backgroundImage,
+            description,
+            brand,
+            images
+        } = req.body;
+
+        
+
+        try {
+
+            let shop_db = await Shop.findOne({account: accountId});
+
+            if(role === "owner" && shop_db){
+                if(!brand){
+                    return res
+                    .status(400)
+                    .json({
+                        success: false,
+                        message: "Không được để trống trường này."
+                    });
+                } 
+
+                const fm_brand = brand.replace(/\s+/g,' ').trim();
+                
+               shop_db = await Shop.findOne({brand: fm_brand});
+                if(shop_db){
+                    return res
+                    .status(400)
+                    .json({
+                        success: false,
+                        message: "Thương hiệu này đã tồn tại."
+                    });
+                }
+
+                const updateData = {
+                    brand: fm_brand,
+                    alias: Format.alias(fm_brand),
+                    backgroundImage,
+                    images,
+                    avatar,
+                    description
+                }
+
+                const updateShop = await Shop.findOneAndUpdate(
+                    {account: accountId},
+                    updateData,
+                    {new: true}
+                );
+
+
+                return res
+                    .json({
+                        success: true,
+                        message: "Thành công",
+                        shop: updateShop
+                    });
+
+            }
+
+            return res
+            .status(400)
+            .json({
+                success: false,
+                message: "Bạn không có quyền truy cập tại đường link này."
+            });
+
+        } catch (error) {
+            console.log(error)
+            return res
+            .status(500)
+            .json({
+                success: false,
+                message: "Internal server error"
+            });
+        }
+    }
 }
--- routes/shop.js
+++ routes/shop.js
@@ -3,12 +3,20 @@ const router = express.Router();
 const verifyToken = require('../middleware/auth');
 const shopController = require('../controllers/shop')
 
-// ================= POST ==================
+// ================= GET ==================
 
-// @route POST api/shop
+// @route GET api/shop
 // @desc get information of shop
 // @access Private
 router.get('/', verifyToken, shopController.getInformation);
+
+
+// ================= POST ==================
+
+// @route POST api/shop
+// @desc put information of shop
+// @access Private
+router.post('/', verifyToken, shopController.updateInformation);
 
 
 
